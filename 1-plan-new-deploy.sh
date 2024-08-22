4a6e4a5b3e0cb3994af8657cb2e6ab5120ed4c8b
Aaron Smith <aasmith@redhat.com>
2024-08-22 01:44:44 +0000

    add 3 third after removing OSPdO
    Signed-off-by: Aaron Smith <aasmith@redhat.com>

--- 1-plan-new-deploy.sh
+++ 1-plan-new-deploy.sh
@@ -50,6 +50,8 @@ OSP18_NODE1=$(echo "${NG_NODES}" | cut -d ' ' -f 1)
 export OSP18_NODE1
 OSP18_NODE2=$(echo "${NG_NODES}" | cut -d ' ' -f 2)
 export OSP18_NODE2
+OSP18_NODE3=$(echo "${NG_NODES}" | cut -d ' ' -f 3)
+export OSP18_NODE3
 
 echo "Label nodes..."
 oc label nodes "${OSP18_NODE1}" type=openstack || {
